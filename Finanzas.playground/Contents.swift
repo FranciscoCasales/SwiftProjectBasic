dd25b5b871408135493853db7dba8d348f0fd254
Jose Francisco Casales Huerta <jcasalesh1200@alumno.ipn.mx>
2019-01-14 16:40:48 -0600

    Primera prueba con estructuras

--- Finanzas.playground/Contents.swift
+++ Finanzas.playground/Contents.swift
@@ -1,5 +1,55 @@
-//: Playground - noun: a place where people can play
+import Foundation
 
-import UIKit
+struct Account {
+    var amount: Float = 0
+    var bank: String = ""
+    var transaction: [Float] = []
+    
+    init(amount: Float, bank: String){
+        self.amount = amount
+        self.bank = bank
+    }
+    
+    @discardableResult
+    mutating func addTransaction(value: Float) -> Float{
+        if (amount-value)<0{
+            return 0
+        }
+        amount -= value
+        transaction.append(value)
+        return amount
+    }
+}
 
-var str = "Hello, playground"
+struct Person {
+    var name: String
+    var lastName: String
+    var account: Account?
+}
+
+var me = Person(name: "Jose", lastName: "Casales", account: nil)
+
+let account = Account(amount: 100_00, bank: "Bank")
+
+me.account = account
+
+print(me.account!.amount)
+
+me.account!.addTransaction(value: 20)
+
+print(me.account!.amount)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
